ddda99d9a2946519472d4b53f42857b92defd80b
JediJake66 <jacobstout59@gmail.com>
2022-01-26 03:37:41 -0500

    added home and contact items with functionality

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,1 +1,31 @@
-console.log('Client Side is wired up!');
+import Contact from "./components/Contact"
+import Header from "./components/Header"
+import Home from "./components/Home"
+
+buildPage();
+
+function buildPage(){
+    header();
+    navContact();
+    navHome();
+    
+}
+function header() {
+    const headerElem = document.querySelector('.header');
+    headerElem.innerHTML = Header();
+  }
+  function navHome() {
+    const homeElem = document.querySelector('.nav-list__home');
+    homeElem.addEventListener('click', () => {
+      const app = document.querySelector('#app');
+      app.innerHTML = Home();
+    });
+  }
+
+function navContact() {
+    const contactElem = document.querySelector('.nav-list__contact');
+    contactElem.addEventListener('click', () => {
+      const app = document.querySelector('#app');
+      app.innerHTML = Contact();
+    });
+  }
