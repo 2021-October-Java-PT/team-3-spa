fe369802564dbd282a532a8d40ec70b71a5734a6
JediJake66 <jacobstout59@gmail.com>
2022-01-26 22:16:24 -0500

    created skeleton for model repo and controller

---
+++ server-side/src/main/java/org/wecancodeit/serverside/Populator.java
@@ -0,0 +1,25 @@
+package org.wecancodeit.serverside;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.wecancodeit.serverside.repositories.BodyOfWaterRepo;
+
+import javax.annotation.Resource;
+
+@Component
+public class Populator implements CommandLineRunner {
+
+    @Resource
+    private BodyOfWaterRepo bodyOfWaterRepo;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        
+
+
+
+
+
+    }
+}
---
+++ server-side/src/main/java/org/wecancodeit/serverside/repositories/BodyOfWaterRepo.java
@@ -0,0 +1,7 @@
+package org.wecancodeit.serverside.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.wecancodeit.serverside.models.BodyOfWater;
+
+public interface BodyOfWaterRepo extends CrudRepository<BodyOfWater, Long> {
+}
