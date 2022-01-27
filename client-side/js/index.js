af071c707c2de3609fd465b14d685e1eda8a4b3e
MartelBarkley <martel.barkley@gmail.com>
2022-01-26 16:38:17 -0800

    Random

--- client-side/js/index.js
+++ client-side/js/index.js
@@ -1,1 +1,5 @@
 console.log('Client Side is wired up!');
+// fetch('http://www.boredapi.com/api/activity?key=5881028')
+//     .then(response => response.json())
+//     .then(jsonData => console.log(jsonData))
+//     .catch(err => console.log(err))
