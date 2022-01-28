8040735d6e708b8c1238124abbe6dbcf9a8af1d8
Hesan Haghnazar <hesan.crec@gmail.com>
2022-01-28 01:17:03 -0500

    this adds info blocks to the contact page

--- client-side/js/components/About.js
+++ client-side/js/components/About.js
@@ -4,11 +4,17 @@ export default function About(){
     return `
     <h1>Our Team</h1>
     <ul class='team-list'>
-        <li class='jake'>Jacob Stout</li>
+        <li class='jake'>Jacob Stout</li>        
+        <p class="about_details">Info about Jake</p>
         <li class='martel'>Martel Barkley</li>
+        <p class="about_details">Info about Martel</p>
         <li class='mike'>Michael Johnson</li>
+        <p class="about_details">Info about Michael</p>
         <li class='hesan'>Hesan Haghnazar</li>
+        <p class="about_details">Info about Hesan</p>
         <li class='cam'>Cameron Stephens</li>  
-    
+        <p class="about_details">Info about Cameron</p>
+    </ul>
+ 
     `;
 }
