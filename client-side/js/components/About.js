1249fd0ccc5609e0612a9cfa710a4c04d7638290
Hesan Haghnazar <hesan.crec@gmail.com>
2022-01-30 10:52:21 -0500

    this is the same touchup but with the current version code

--- client-side/js/components/About.js
+++ client-side/js/components/About.js
@@ -3,12 +3,17 @@
 export default function About(){
     return `
     <h1>Our Team</h1>
-    <ul class='team-list'>
-        <li class='jake'>Jacob Stout</li>
-        <li class='martel'>Martel Barkley</li>
-        <li class='mike'>Michael Johnson</li>
-        <li class='hesan'>Hesan Haghnazar</li>
-        <li class='cam'>Cameron Stephens</li>  
-    
+    <ul class='about_name'>
+    <li class='about_name'>Jacob Stout</li>        
+    <p class="about_details">Info about Jake</p>
+    <li class='about_name'>Martel Barkley</li>
+    <p class="about_details">Info about Martel</p>
+    <li class='about_name'>Michael Johnson</li>
+    <p class="about_details">Info about Michael</p>
+    <li class='about_name'>Hesan Haghnazar</li>
+    <p class="about_details">Info about Hesan</p>
+    <li class='about_name'>Cameron Stephens</li>  
+    <p class="about_details">Info about Cameron</p>
+    </ul>
     `;
 }
