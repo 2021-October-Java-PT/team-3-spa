01b31e9419e84241e9f083f27fe03febd718fcb6
jordanCCIT <jordanadposting@gmail.com>
2022-01-30 13:21:04 -0500

    fixed error that was stopping code from running

--- server-side/src/main/java/org/wecancodeit/serverside/repositories/CommentsRepo.java
+++ server-side/src/main/java/org/wecancodeit/serverside/repositories/CommentsRepo.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 
 
 public interface CommentsRepo extends CrudRepository<Comments, Long> {
+    //This was causing a problem, review before uncommenting!
 //    Optional<Comments> findByName(String commentsName);
 }
