fafedf5f2ce6c5dca497175b015d1273c4c28ff3
Hesan Haghnazar <hesan.crec@gmail.com>
2022-02-03 13:14:43 -0500

    this adds a marine life button

--- client-side/js/components/Header.js
+++ client-side/js/components/Header.js
@@ -3,6 +3,8 @@ export default function Header() {
     <ul class='nav-list'>
         <li class='nav-list__home'><button>Home</button></li>
         <li class='nav-list__activities'><button>Activities</li>
+        <li class='nav-list__activities2'><button>Marine Life</li>
+        
         <li class='nav-list__about'><button>About The Team</button></li>
         <li class='nav-list__contact'><button>Contact Us</button></li>
         
